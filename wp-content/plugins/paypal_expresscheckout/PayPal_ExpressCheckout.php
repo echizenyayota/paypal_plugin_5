77df1e24c176cd8e1e191c03e1932bc1c116a5b2
echizenyayota <virtualmisso30@gmail.com>
2018-07-11 10:27:08 +0900

    設定ページの表示画面を作成

--- wp-content/plugins/paypal_expresscheckout/PayPal_ExpressCheckout.php
+++ wp-content/plugins/paypal_expresscheckout/PayPal_ExpressCheckout.php
@@ -35,7 +35,35 @@ Text Domain: paypal_expresscheckout
 
   // 設定ページの表示
   public function create_admin_page() {
+    // my_option_nameをoptionsのプロパティとする
+  $this->options = get_option( 'paypl_option_name' );
+  ?>
+  <div class="wrap">
+     <h2>PayPal ExpressCheckout Settings Page</h2>
+     <form method="post" action="options.php">
+       <?php settings_fields( 'paypal-settings-group' ); ?>
+       <?php do_settings_sections( 'paypal-settings-group' ); ?>
+       <table class="form-table">
+         <tr>
+           <th scope="row">Develop Enviroment</th>
+           <td>
+             <p>
+               <select name="env" size="1">
+                 <option value="sandbox" <?php selected( get_option( 'env' ), 'sandbox' ); ?>>sandbox</option>
+                 <option value="production" <?php selected( get_option( 'env' ), 'production' ); ?>>production</option>
+               </select>
+             </p>
+           </td>
+         </tr>
+         <tr valign="top">
+         <th scope="row">Client ID</th>
+         <td><input type="text" name="client" size="90" value="<?php echo esc_attr( get_option('client') ); ?>" /></td>
+         </tr>
+       </table>
+       <?php submit_button(); ?>
+     </form>
 
+  <?php
 
   }
 
